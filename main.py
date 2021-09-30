49c6491d994c4d401efda366e94ccca3b9046ab4
drewcapener <drewcapener@github.com>
2021-09-30 15:14:21 -0600

    Added ProfScrapeService w/ login functionality

--- main.py
+++ main.py
@@ -6,13 +6,18 @@
 # for the given study.
 from scraper import Scraper
 from frreqscrapeservice import FrReqScrapeService
+from profscrapeservice import ProfScrapeService
 
 # Initialize the scraper with the credentials for the
 # study participant (one-time passwords only!!!)
-scraper = Scraper('8888888888', 'fakePassword')
+# Here, we read the credentials from env vars so that creds
+# aren't being pushed to the public repo
+import os
+scraper = Scraper(os.environ['PHONE'], os.environ['FB_PWD'])
 
 # Attach the scrape services you'll be using.
 scraper.attach(FrReqScrapeService())
+scraper.attach(ProfScrapeService())
 
 # Call scrape to gather all data
 scraper.scrape()
---
+++ profscrapeservice.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+from iscrapeservice import IScrapeService
+
+class ProfScrapeService(IScrapeService):
+
+    def scrape(self, user_dto):
+        
+        phone_number = user_dto.phone_number
+        password = user_dto.password
+        
+        option = Options()
+        option.add_argument('--disable-notifications')
+        browser = webdriver.Chrome(options=option)
+        browser.get('https://www.facebook.com/')
+
+        username_input = browser.find_element_by_name('email')
+        password_input = browser.find_element_by_name('pass')
+        login_button = browser.find_element_by_name('login')
+
+        # notice we are using username here but it will be phone number eventually
+        username_input.send_keys(phone_number)
+        password_input.send_keys(password)
+        login_button.click()
+        time.sleep(2)
+        browser.quit()
+        
+
+
