e74525040e55853964c1455833782fb029b42a11
lucasamonrc <lucasamonrc@gmail.com>
2022-05-13 15:31:04 -0600

    rename driver to browser

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -22,7 +22,7 @@ class Scraper():
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
         browser.get('https://www.facebook.com/')
          
